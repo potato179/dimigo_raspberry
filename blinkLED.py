b18d77a13058829ad458c5c915fa32c8a3713fcb
홍은표 <44293278+potato179@users.noreply.github.com>
2022-05-04 10:59:41 +0900

    Update blinkLED.py

--- blinkLED.py
+++ blinkLED.py
@@ -1,12 +1,31 @@
 import RPi.GPIO as GPIO
 import time
 
+PIN_RED = 17
+PIN_YELLOW = 27
+PIN_GREEN = 22
 GPIO.setmode(GPIO.BCM)
-LED_PIN = 4
-GPIO.setup(LED_PIN, GPIO.OUT)
+GPIO.setup(PIN_RED, GPIO.OUT)
+GPIO.setup(PIN_YELLOW, GPIO.OUT)
+GPIO.setup(PIN_GREEN, GPIO.OUT)
 
-while True : 
-    GPIO.output(LED_PIN, True)
-    time.sleep(0.1)
-    GPIO.output(LED_PIN, False)
-    time.sleep(0.1)
+for i in range(99999999999999):
+    GPIO.output(PIN_RED, GPIO.HIGH)
+    GPIO.output(PIN_YELLOW, GPIO.LOW)
+    GPIO.output(PIN_GREEN, GPIO.LOW)
+    print("RED ON")
+    time.sleep(0.05)
+    GPIO.output(PIN_RED, GPIO.LOW)
+    GPIO.output(PIN_YELLOW, GPIO.HIGH)
+    GPIO.output(PIN_GREEN, GPIO.LOW)
+    print("YELLOW ON")
+    time.sleep(0.05)
+    GPIO.output(PIN_RED, GPIO.LOW)
+    GPIO.output(PIN_YELLOW, GPIO.LOW)
+    GPIO.output(PIN_GREEN, GPIO.HIGH)
+    print("GREEN ON")
+    time.sleep(0.05)
+
+GPIO.setup(PIN_RED, GPIO.OUT)
+GPIO.setup(PIN_YELLOW, GPIO.OUT)
+GPIO.setup(PIN_GREEN, GPIO.OUT)
