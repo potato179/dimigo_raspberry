d2edc051a23b6ac7ef962834233634b70f66d06e
홍은표 <44293278+potato179@users.noreply.github.com>
2022-05-19 16:09:12 +0900

    Update airplanesound.py

--- airplanesound.py
+++ airplanesound.py
@@ -11,5 +11,5 @@ pwm.start(99)
 
 for i in range(10000000000000000000000000000000000000000000000000000):
     pwm.ChangeFrequency(i+1)
-    print(i+1)
-    time.sleep(0.000001)
+    print("BJS speed", i+1, "km/h")
+    time.sleep(0.00000001)
