90faec1d0a7beef230c2eaf89746c11f910073df
홍은표 <44293278+potato179@users.noreply.github.com>
2022-06-23 15:56:58 +0900

    Update sound.py

--- sound.py
+++ sound.py
@@ -9,12 +9,12 @@ GPIO.setup(BUZZER_pin, GPIO.OUT)
 pwm = GPIO.PWM(BUZZER_pin, 262)
 pwm.start(99)
 
-for i in range(100000):
+for i in range(1000000):
     pwm.ChangeFrequency(i+1)
     print(i+1)
     time.sleep(0.00001)
 
-for i in range(100000):
-    pwm.ChangeFrequency(100000-i)
-    print(100000-i)
+for i in range(1000000):
+    pwm.ChangeFrequency(1000000-i)
+    print(1000000-i)
     time.sleep(0.00001)
