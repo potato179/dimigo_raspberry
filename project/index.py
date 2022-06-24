0630c1c48d794aa902729b3021e9a85b9bab3d10
홍은표 <44293278+potato179@users.noreply.github.com>
2022-06-24 10:26:02 +0900

    Update index.py

--- project/index.py
+++ project/index.py
@@ -1,32 +1,6 @@
 import RPi.GPIO as GPIO
 import time
 
-SEGMENT_PINS = [3, 4, 5, 6, 7, 12, 13]
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-
-for segment in SEGMENT_PINS:
-    GPIO.setup(segment, GPIO.OUT)
-    GPIO.output(segment, 0)
-
-data = [1, 1, 1, 1, 1, 1, 0]
-
-try:
-    for _ in range(3):
-        for i in range(len(SEGMENT_PINS)):
-            GPIO.output(SEGMENT_PINS[i], data[i])
-
-        time.sleep(1)
-
-        for i in range(len(SEGMENT_PINS)):
-            GPIO.output(SEGMENT_PINS[i], GPIO.LOW)
-
-        time.sleep(1)
-finally:
-    GPIO.cleanup()
-    print('clean up and exit')
-
 PIN_RED = 17
 PIN_YELLOW = 27
 PIN_GREEN = 22
