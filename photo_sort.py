26d73b5d8455c5714d0e3d9ba35d124c6d66bd1f
홍은표 <44293278+potato179@users.noreply.github.com>
2022-09-26 10:11:42 +0900

    Update photo_sort.py

--- photo_sort.py
+++ photo_sort.py
@@ -11,9 +11,9 @@ with open(classFile, "rt") as f:
     classNames = f.read().rstrip("\n").split("\n")
 
 net = cv2.dnn.readNet(model, config)
-img = cv2.imread("gay.jpg")
+img = cv2.imread("haengsin.jpeg")
 
-blob = cv2.dnn.blobFromImage(img, scalefactor = 1, size = (160, 90), mean = (104, 117, 123))
+blob = cv2.dnn.blobFromImage(img, scalefactor = 1, size = (224, 224), mean = (104, 117, 123))
 
 net.setInput(blob)
 detections = net.forward()
@@ -23,7 +23,9 @@ classId = np.argmax(out)
 confidence = out[classId]
 
 text = "%s (%4.2f%%)" % (classNames[classId], confidence * 100)
+img = cv2.resize(img, (600, 400))
 cv2.putText(img, text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 1, cv2.LINE_AA)
+
 cv2.imshow("img", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
