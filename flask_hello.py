54129a71a34583f404f218bd04e9cf6b3d66b426
홍은표 <44293278+potato179@users.noreply.github.com>
2022-10-11 10:11:03 +0900

    Update flask_hello.py

--- flask_hello.py
+++ flask_hello.py
@@ -3,11 +3,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<h1>Flask, trash!</h1>"
+    return "<h1>Flask, trash!</h1><br><a href = '/first'>I love Express</a>"
 
 @app.route("/first")
 def express_mansae():
-    return "<h1>Express 만세!</h1>"
+    return "<h1>Express 만세!</h1><br><a href = '/'>돌아가</a>"
 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0")
