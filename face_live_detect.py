d3da5743ec05a45368926c2d6a178870e21e0850
홍은표 <44293278+potato179@users.noreply.github.com>
2022-10-17 10:36:40 +0900

    Update face_live_detect.py

--- face_live_detect.py
+++ face_live_detect.py
@@ -19,11 +19,14 @@ while True:
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
     # 얼굴 위치에 대한 좌표 정보 가져오기
-    print(x, y, w, h)
     for (x, y, w, h) in faces:
         # 원본이미지에 얼굴 위치 표시
         # (x,y) 에서 시작, 끝점(x+가로), (y+세로), BGR색, 굵기 2
         cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 2)
+        if(x):
+            print("True")
+        else:
+            print("False")
 
     cv2.imshow('img', frame)
     if cv2.waitKey(10) == 27:
