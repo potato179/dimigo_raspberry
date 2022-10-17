84c15cda8940cdfdb58aaeeb2bc0e631d9e247b4
홍은표 <44293278+potato179@users.noreply.github.com>
2022-10-17 09:21:44 +0900

    led

---
+++ flask_led.py
@@ -0,0 +1,33 @@
+from flask import Flask
+import RPi.GPIO as GPIO 
+
+PIN_LED = 19
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(PIN_LED, GPIO.OUT)
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return '''<p>Hello, Flask!</p>
+        <a href = '/led/on'> on</a>
+        <a href = '/led/off'> off</a>
+        <a href = '/asdf'> asdf</a>
+    '''
+
+@app.route("/led/on")
+def on():
+    GPIO.output(PIN_LED, True)
+    return "on page <a href = '/'> home</a>"
+
+@app.route("/led/off")
+def off():
+    GPIO.output(PIN_LED, False)
+    return "off page <a href = '/'> home</a>"
+
+@app.route("/asdf")
+def asdf():
+    return "asdf page <a href = '/'> home</a>"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
