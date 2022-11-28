0ca204aae19d866d0071d377ba56df2fa55afc94
홍은표 <44293278+potato179@users.noreply.github.com>
2022-11-28 09:57:48 +0900

    뭉탱이

--- flask_hello.py
+++ flask_hello.py
@@ -1,13 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<h1>Flask, trash!</h1><br><a href = '/first'>I love Express</a>"
+    return render_template("hello.html")
 
 @app.route("/first")
 def express_mansae():
     return "<h1>Express 만세!</h1><br><a href = '/'>돌아가</a>"
+
+@app.route("/led")
+def ledjoygo():
+    return render_template("led.html")
 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0")
